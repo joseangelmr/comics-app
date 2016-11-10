882e735760c1e34790b206667facb073c3f08489
Jose Marquez <joseangel2212@gmail.com>
2016-11-10 11:18:01 -0400

    feat(config): added states support to app

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,37 @@
-angular.module('starter', ['ionic'])
+angular.module('app', ['ionic', 'app.controllers', 'app.services', 'ngLodash', 'ionicLazyLoad', 'jett.ionic.filter.bar'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    })
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/sidebar.html",
+        controller: 'AppCtrl'
+      })
+
+
+      .state('app.comics', {
+        url: "/comics",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/comics.html",
+            controller: 'DiscoverCtrl'
+          }
+        }
+      })
+
+    $urlRouterProvider.otherwise('/app/comics');
   });
-})
